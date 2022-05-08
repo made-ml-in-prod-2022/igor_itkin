a1d62f8de054ce1a67b3b6b8bd2e474052126387
Igor (Yehuda) Itkin <igori@codefuel.com>
2022-05-08 16:12:13 +0300

    adding configs for hydra

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
-    pass
+import logging
+import os
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+logger = logging.getLogger(__name__)
+
+
+@hydra.main(config_path="configs", config_name="config")
+def main(cfg: DictConfig) -> None:
+    logger.info(OmegaConf.to_yaml(cfg))
+    logger.info("Working directory : {}".format(os.getcwd()))
 
 
 if __name__ == '__main__':
