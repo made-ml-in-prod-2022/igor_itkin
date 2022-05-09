3a5e44dd1078442dca019e4d28b48ace096fc2da
Igor (Yehuda) Itkin <ig.itkin@gmail.com>
2022-05-09 09:05:28 +0300

    merge and update ci

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import logging
 import os
 
 import hydra
+from hydra.utils import instantiate
+import numpy as np
+
 from omegaconf import DictConfig, OmegaConf
 
 logger = logging.getLogger(__name__)
@@ -11,6 +14,8 @@ logger = logging.getLogger(__name__)
 def main(cfg: DictConfig) -> None:
     logger.info(OmegaConf.to_yaml(cfg))
     logger.info("Working directory : {}".format(os.getcwd()))
+    data_reader = instantiate(cfg.data_reader)
+
 
 
 if __name__ == '__main__':
