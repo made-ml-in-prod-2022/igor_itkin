3ee36dbc77c91f3ea2d864c5788909a2f8efc1f3
Igor (Yehuda) Itkin <igori@codefuel.com>
2022-05-09 12:24:44 +0300

    Data reader

--- src/data/data_reader.py
+++ src/data/data_reader.py
@@ -13,9 +13,19 @@ class DataReader:
     categorical: list
     target: str
 
-    def read_data(self, path: str) -> pd.DataFrame:
+    def read_data(self, path: str) -> (pd.DataFrame, pd.Series):
         df = pd.read_csv(path)
-
-        for catdata in self.categorical:
+        names2drop = []
+        new_dfs = []
+        for catdata in data_reader.categorical:
             name = catdata['name']
-            df[name] = OneHotEncoder(categories=catdata['values']).fit_transform(df[name])
+            names2drop.append(name)
+            data = df[name].values.reshape(-1, 1)
+            transformed = pd.DataFrame(OneHotEncoder(categories=[catdata['values']], sparse=False).fit_transform(data))
+            transformed.columns = [f"{name}_{value}" for value in catdata['values']]
+            new_dfs.append(transformed)
+        df = df.drop(names2drop, axis=1, errors='ignore')
+        df = pd.concat((df, *new_dfs), axis=1)
+        target = df[self.target] if self.target in df.columns else None
+        df.drop(self.target, axis=1, inplace=True, errors='ignore')
+        return df, target
