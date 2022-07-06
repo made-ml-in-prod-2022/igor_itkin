fdfeb4fc803e328d8f4be4fca96e796930081062
Igor (Yehuda) Itkin <igori@codefuel.com>
2022-07-06 13:29:57 +0300

    preprocess

---
+++ howework3/airflow/images/airflow-preprocess/preprocess.py
@@ -0,0 +1,28 @@
+import os
+import pickle
+
+import click
+import pandas as pd
+
+INPUT_FILE = "features_train.csv"
+OUTPUT_FILE = "target_train.csv"
+MODEL_FILE = "model.pkl"
+
+
+@click.command("predict")
+@click.option("--model-dir")
+@click.option("--input-dir")
+@click.option("--output-dir")
+def predict(model_dir:str, input_dir: str, output_dir:str):
+    features = pd.read_csv(os.path.join(input_dir, INPUT_FILE))
+    model_fullpath = os.path.join(model_dir, MODEL_FILE)
+    with open(model_fullpath, "rb") as f:
+        model = pickle.load(f)
+    predictions = model.predict(features)
+
+    os.makedirs(output_dir, exist_ok=True)
+    predictions.to_csv(os.path.join(output_dir, OUTPUT_FILE))
+
+
+if __name__ == '__main__':
+    predict()
